d30ef5bf2f94494de521d8bb335085faa76fe08d
jhoward-embedded <ros71745@gmail.com>
2026-08-05 11:20:53 +0800

    New update

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/piso-print
+ip=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 echo "LCC PISO PRINTER starting..."
 echo "Open on tablet: http://127.0.0.1:3000"
+echo "Open on any device: http://"$ip":3000"
 echo "Press Ctrl+C to stop"
 node server.js
